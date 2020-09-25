ec37a76e4836b56dbe31e68e005aab96d92faa9d
Austin Lambert <atlambert11@gmail.com>
2020-09-24 19:40:51 -0500

    Created function that would return values from the 4 arrays I created.

--- Develop/script.js
+++ Develop/script.js
@@ -13,13 +13,12 @@ var alphLowConfirm = confirm("Do you want lower case letters in your password?")
 var alphUpperConfirm = confirm("Do you want uppercase letters in your password?");
 var numbersConfirm = confirm("Do you want numbers in your password?");
 var specCharConfirm = confirm("Do you want special characters in your password?");
+var charLength = confirm("How many characters do you want your password to be? (Must be between 8 and 128 characters.")
 
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword(arr) {
-
-    };
+    var password = generatePassword(arr);
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
@@ -32,9 +31,22 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+function generatePassword(arr) {
+    for (var i = 0; i < arr.length; i++) {
 
-// Call for writePassword function to run for-loop on each array.
-writePassword(alphabetLower);
-writePassword(alphabetUpper);
-writePassword(numbers);
-writePassword(specCharacters);
+        console.log(arr[i]);
+
+    }
+    console.log("--------------------------")
+
+
+
+
+}
+
+
+// Call for generatePassword function to run for-loop on each array.
+generatePassword(alphabetLower);
+generatePassword(alphabetUpper);
+generatePassword(numbers);
+generatePassword(specCharacters);
